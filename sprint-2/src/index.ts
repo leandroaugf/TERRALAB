2306beb0e498e619cbd976726c58ee25a45dc0a2
leandroaugf <leandroaugf@gmail.com>
2026-03-20 11:45:24 -0300

    sprint 2 fix

--- sprint-2/src/index.ts
+++ sprint-2/src/index.ts
@@ -7,8 +7,8 @@ import { withdraw } from './models/bankAccount'
 
 import { createUser } from './models/user';
 
-const userr = createUser({ id: "", age: 21, name: 'John Doe', email: "johndoe@icloud.com" });
-const bankAccount = createBankAccount(userr, { agency: '0001', number: 1 });
+const user = createUser({ id: "", age: 21, name: 'John Doe', email: "johndoe@icloud.com" });
+const bankAccount = createBankAccount(user, { agency: '0001', number: 1 });
 
 
 deposit(bankAccount, 1000);
